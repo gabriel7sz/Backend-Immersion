b9d2c3e1833e0b7ef5e4b0fcdce32c908191c268
gabriel7sz <gabriel7sz@outlook.com>
2024-11-19 11:09:13 -0300

    aula2

--- server.js
+++ server.js
@@ -3,6 +3,42 @@
  * foi instalado o arquivo express
  * */ 
 import express from "express";
+
+// Criando um array
+const posts = [
+    // criando um objeto
+    {
+        id: 1,
+        descricao: "Uma foto teste", 
+        imagem: "https://placecats.com/millie/300/150"
+    } ,
+    {
+        id: 2,
+        descricao: "Gato fazendo yoga",
+        imagem: "https://placecats.com/millie/300/150"
+    } ,
+    {
+        id: 3,
+        descricao: "Gatinho dormindo",
+        imagem: "https://placecats.com/millie/300/150"
+    } ,
+    {
+        id: 4,
+        descricao: "Receita deliciosa de bolo de chocolate!",
+        imagem: "https://placecats.com/millie/300/150"
+    } ,
+    {
+        id: 5,
+        descricao: "Meu novo cachorro",
+        imagem: "https://placecats.com/millie/300/150"
+    } ,
+    {
+        id: 6,
+        descricao: "Alpes suíços",
+        imagem: "https://placecats.com/millie/300/150"
+    } 
+] ;
+
 /**
  * iniciando express
  * Express é uma função que quando for executada passará o resultado
@@ -10,6 +46,10 @@ import express from "express";
  * Como se app representasse o servidor
  * */ 
 const app = express();
+
+// parssear o texto do array em json
+app.use(express.json());
+
 /**
  * Fazendo o servidor 'SUBIR'
  * Esta função recebe dois parâmetros.
@@ -22,6 +62,22 @@ app.listen(3000, () => {
  * Criando um caminho para o computador cliente recuperar dados.
  * 
  * */ 
-app.get("/api", (req, res) => {
-    res.status(200).send("Boas Vindas a imersão");
+app.get("/posts", (req, res) => {
+    res.status(200).json(posts);
+});
+
+// criando uma função que busque por ids dentro do array
+function buscarPostPorID(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id)
+    })
+}
+
+/**
+ * O que siginifica esses dois pontos?
+ * Os ':' diz para o express que esta informação quando for no navegador, será subistítuida por um dando variável.
+ * */ 
+app.get("/posts/:id", (req, res) => {
+    const index = buscarPostPorID(req.params.id)
+    res.status(200).json(posts[index]);
 });
